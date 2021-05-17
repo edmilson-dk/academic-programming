673071e63ff54e50660af9ebf7006b0456938254
edmilson-dk <droidvidaboa@gmail.com>
2021-05-17 20:38:51 -0300

    refactor: added round method

--- search-algorithms/sequential-search/binary-search/binary-search.py
+++ search-algorithms/sequential-search/binary-search/binary-search.py
@@ -3,9 +3,9 @@
 def binarySearch(vector, size, wanted):
     left_index = 0
     right_index = size-1
-
+  
     while left_index <= right_index:
-        pivot = left_index + (right_index - left_index) / 2
+        pivot = round(left_index + (right_index - left_index) / 2)
 
         if vector[pivot] == wanted: return pivot
         elif vector[pivot] < wanted: left_index = pivot+1
@@ -13,6 +13,6 @@ def binarySearch(vector, size, wanted):
 
     return -1
 
-vector = [1, 2, 3, 4, 5, 6]
+vector = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-print(binarySearch(vector, 6, 4))
+print(binarySearch(vector, 9, 7))
--- search-algorithms/sequential-search/binary-search/recursive-binary-search.py
+++ search-algorithms/sequential-search/binary-search/recursive-binary-search.py
@@ -2,7 +2,7 @@
 
 def recursiveBinarySearch(vector, left_index, right_index, wanted):
   if left_index <= right_index:
-    pivot = left_index + (right_index - left_index) / 2
+    pivot = round(left_index + (right_index - left_index) / 2)
 
     if (vector[pivot] == wanted): return pivot
     elif vector[pivot] < wanted: 
